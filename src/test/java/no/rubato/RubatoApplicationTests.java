8887fd16f647cc73c71c3eb77f9c04e2ca608612
Zain Qureshi <zainqureshi145@gmail.com>
2019-08-29 18:57:36 +0200

    Version-0.5

---
+++ src/test/java/no/rubato/RubatoApplicationTests.java
@@ -0,0 +1,16 @@
+package no.rubato;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RubatoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
